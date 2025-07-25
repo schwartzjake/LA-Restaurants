09b2d66410e80f4fdcca49933ae3603203dc860d
schwartzjake <jake.s.schwartz@gmail.com>
2025-07-25 13:42:04 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -5,3 +6,17 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* PNG alternative: */}
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+        {/* Apple touch icon: */}
+        {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}

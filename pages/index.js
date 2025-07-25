828f0a251482566d10cd662897c7be4ac72ae92e
schwartzjake <jake.s.schwartz@gmail.com>
2025-07-25 13:15:56 -0700

    Larger LA

--- pages/index.js
+++ pages/index.js
@@ -125,7 +125,7 @@ export default function Home() {
 
      <h1 className="text-5xl font-bold uppercase tracking-tight mb-10 flex items-center gap-2">
   The List
-  <span className="bg-[#592025] text-[#F2F2F2] px-2 py-1 text-xl rounded">
+  <span className="bg-[#592025] text-[#F2F2F2] px-2 py-1 text-3xl rounded">
     LA
   </span>
 </h1>
